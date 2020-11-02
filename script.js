eed43cc9f7d56036a65a15119ddb07a28795e616
Daniel <mineminecraftpe687@gmail.com>
2020-11-03 10:57:09 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let username, project, url;
+async function submit() {
+  project = encodeURIComponent(document.getElementById('txt1').value);
+  username = encodeURIComponent(document.getElementById('txt2').value);
+  if(!project || !username) {
+    alert('That project does not exist');
+    return;
+  }
+  const req = await fetch(`https://cors-anywhere.herokuapp.com/https://snap.berkeley.edu/projects/${username}/${project}`);
+  if(req.ok) {
+    loadProject();
+  } else if(req.status === 404){
+    alert('That project does not exist');
+  }
+}
+function loadProject() {  
+  url=`https://snap.berkeley.edu/embed?project=${project}&user=${username}&showTitle=true&showAuthor=true&editButton=true&pauseButton=true`;
+  document.getElementById('if').src = url;
+  document.getElementById('code').value = url;
+}
